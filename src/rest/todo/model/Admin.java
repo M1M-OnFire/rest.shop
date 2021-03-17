5271e54a8f77e3c8a5b0df6ba98f62f2d0ed2b0f
Ardenne chakir <ardenne.chakir@gmail.com>
2021-03-17 20:41:28 +0100

    feat: gestion Admin

---
+++ src/rest/todo/model/Admin.java
@@ -0,0 +1,42 @@
+package rest.todo.model;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Admin {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = hashPassword(password);
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static String hashPassword(String password){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            return DatatypeConverter.printHexBinary(md.digest()).toUpperCase();
+        }
+        catch (NoSuchAlgorithmException exception) {
+            throw new RuntimeException("Erreur lors du hash du mot de passe");
+        }
+    }
+}
