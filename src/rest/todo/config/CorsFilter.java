dcbe2542f36562dadee12fafcc941b17346b9d65
Ardenne chakir <ardenne.chakir@gmail.com>
2021-03-18 17:09:07 +0100

    fix:  fix filtre cors

--- src/rest/todo/config/CorsFilter.java
+++ src/rest/todo/config/CorsFilter.java
@@ -1,34 +1,70 @@
 package rest.todo.config;
 
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.ws.rs.container.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
 
 
-@WebFilter(asyncSupported = true, urlPatterns = { "/*" })
-public class CorsFilter implements Filter {
+@Provider
+@PreMatching
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "Content-Type");
-        ((HttpServletResponse) response).addHeader("Access-Control-Max-Age", "86400");
-        // pass the request along the filter chain
-        chain.doFilter(request, response);
-    }
-
-    public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
-    }
-
+    /**
+     * Method for ContainerRequestFilter.
+     */
     @Override
-    public void destroy() {
-        // TODO Auto-generated method stub
+    public void filter(ContainerRequestContext request) throws IOException {
+
+        // If it's a preflight request, we abort the request with
+        // a 200 status, and the CORS headers are added in the
+        // response filter method below.
+        if (isPreflightRequest(request)) {
+            request.abortWith(Response.ok().build());
+            return;
+        }
+    }
+
+    /**
+     * A preflight request is an OPTIONS request
+     * with an Origin header.
+     */
+    private static boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null
+                && request.getMethod().equalsIgnoreCase("OPTIONS");
+    }
+
+    /**
+     * Method for ContainerResponseFilter.
+     */
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response)
+            throws IOException {
+
+        // if there is no Origin header, then it is not a
+        // cross origin request. We don't do anything.
+        if (request.getHeaderString("Origin") == null) {
+            return;
+        }
+
+        // If it is a preflight request, then we add all
+        // the CORS headers here.
+        if (isPreflightRequest(request)) {
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                    // Whatever other non-standard/safe headers (see list above)
+                    // you want the client to be able to send to the server,
+                    // put it in this list. And remove the ones you don't want.
+                    "X-Requested-With, Authorization, " +
+                            "Accept-Version, Content-MD5, CSRF-Token, Content-Type");
+        }
+
+        // Cross origin requests can be either simple requests
+        // or preflight request. We need to add this header
+        // to both type of requests. Only preflight requests
+        // need the previously added headers.
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
     }
 }
