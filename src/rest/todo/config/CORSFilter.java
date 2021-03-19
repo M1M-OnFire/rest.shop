4489d8f4e5adc338c02bf0d27de00891dc3167dd
Ardenne chakir <ardenne.chakir@gmail.com>
2021-03-19 12:34:35 +0100

    fix : filtre cors marche enfin

--- src/rest/todo/config/CORSFilter.java
+++ src/rest/todo/config/CORSFilter.java
@@ -1,34 +1,67 @@
 package rest.todo.config;
 
+import javax.servlet.Filter;
 import java.io.IOException;
+import java.util.Enumeration;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @WebFilter(asyncSupported = true, urlPatterns = { "/*" })
 @Provider
-public class CORSFilter implements javax.servlet.Filter {
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "Content-Type");
-        ((HttpServletResponse) response).addHeader("Access-Control-Max-Age", "86400");
-        // pass the request along the filter chain
-        chain.doFilter(request, response);
+public class CORSFilter implements Filter {
+    /**
+     * Default constructor.
+     */
+    public CORSFilter() {
+        // TODO Auto-generated constructor stub
     }
 
-    public void init(FilterConfig fConfig) throws ServletException {
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
         // TODO Auto-generated method stub
     }
 
-    @Override
-    public void destroy() {
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        // pass the request along the filter chain
+        chain.doFilter(request, servletResponse);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
         // TODO Auto-generated method stub
     }
 }
